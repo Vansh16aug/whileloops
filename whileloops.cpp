6856f28130d2adbe6edbc303f6963d351e53471e
Vansh kumar <kumarvansh16aug@gmail.com>
2023-06-29 21:13:09 +0530

    Add files via upload

---
+++ whileloops.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // int i=1;
+    // while(i<=5)
+    // {
+    //     cout<<"Vansh"<<endl;
+    //     i++;
+    // }
+
+    int i=1;
+    do{
+        cout<<"Vansh"<<endl;
+        i++;
+    }
+    while(i<=5);
+    return 0;
+}
